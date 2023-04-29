95f143e39db9c8b9033ba5bf838c2f27fa92981d
kjjlss2 <jgim0417@gmail.com>
2023-04-30 02:35:19 +0900

    달팽이 문제

--- algorithm0429.py
+++ algorithm0429.py
@@ -1,28 +1,47 @@
-nrf = [1, -1, 0, 0]
-ncf = [0, 0, 1, -1]
-def dfs(r, c):
+#!/usr/bin/env python3
+import sys
+
+nrf = [0,1,0,-1]
+ncf = [1,0,-1,0]
+maps = list()
+visited = list()
+def dfs(r, c, d, v):
+    print("-------")
+    for i in range(len(visited)):
+        print(visited[i])
+    print("-------")
+    cnt = 0
     for i in range(4):
         nr = r + nrf[i]
         nc = c + ncf[i]
-        if(0 <= nr < N) and (0 <= nc < N) and (maps[nr][nc] == 1) and (visited[nr][nc] == 0):
-            visited[nr][nc] = 1
-            dfs(nr, nc)
-
+        if (0 <= nr < N) and (0 <= nc < N) and (visited[nr][nc] >= 1):
+            cnt +=1
+        if cnt == 4:
+            return
+    nr = r + nrf[d]
+    nc = c + ncf[d]
+    if (0 <= nr < N) and (0 <= nc < N) and (maps[nr][nc] == 0) and (visited[nr][nc] == 0):
+        visited[nr][nc] = v
+        dfs(nr,nc,d, v+1)
+    else:
+        if d + 1 >= 4:
+            d = 0
+        else:
+            d+=1
+        nr = r + nrf[d]
+        nc = c + ncf[d]
+        if (0 <= nr < N) and (0 <= nc < N) and (maps[nr][nc] == 0) and (visited[nr][nc] == 0):
+            visited[nr][nc] = v
+            dfs(nr,nc,d, v+1)
+    
 
 if __name__ == '__main__':
-    N= int(input)
-    maps= list()
-    visited= list()
+    N = int(input())
     for i in range(N):
         maps.append([])
         visited.append([])
-        line= input()
-        for j in range(len(line)):
-            maps[i].append(int(line[j]))
-            visited[i].append(0)
-    
-    for i in range(N):
         for j in range(N):
-            if maps[i][j] == 0 and visited[i][j] ==0:
-                visited[i][j] ==1
-                dfs(i,j);
+            maps[i].append(0)
+            visited[i].append(0)
+    visited[0][0]=1
+    dfs(0,0,0,2) # 0 -->
